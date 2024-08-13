7782a5d4a4b28653d48333001ba8b2c754d287fb
romario lima <romariolima1006@gmail.com>
2024-08-13 13:58:03 -0300

    auth, token, password hash

--- madr/models.py
+++ madr/models.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import registry, Mapped, mapped_column
 from sqlalchemy import func, DateTime
 
 
-
 table_registry = registry()
 
 
@@ -12,16 +11,16 @@ table_registry = registry()
 class User:
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(init= False, primary_key=True)
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    email: Mapped[str] = mapped_column(unique= True)
+    email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime, 
-        nullable=False, 
-        default=func.now(), 
+        DateTime,
+        nullable=False,
+        default=func.now(),
         onupdate=func.now()
     )
