811f202e73f4b9074b4675e2862b42b57768c301
romario lima <romariolima1006@gmail.com>
2024-08-14 13:48:54 -0300

    crud users

--- madr/app.py
+++ madr/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from madr.routers import auth
+from madr.routers import auth, users
 
 app = FastAPI()
 app.include_router(auth.router)
+app.include_router(users.router)
--- madr/schemas.py
+++ madr/schemas.py
@@ -1,6 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
+
+
+class Message(BaseModel):
+    message: str
