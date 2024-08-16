3d2d6f1c1ae76f3b35e3044029f7819a26616d83
romario lima <romariolima1006@gmail.com>
2024-08-16 14:12:55 -0300

    models Romancista e Livro, tests

--- madr/models.py
+++ madr/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy.orm import registry, Mapped, mapped_column
-from sqlalchemy import func, DateTime
+from sqlalchemy.orm import registry, Mapped, mapped_column, relationship
+from sqlalchemy import func, DateTime, ForeignKey
 
 
 table_registry = registry()
@@ -23,4 +23,28 @@ class User:
         nullable=False,
         default=func.now(),
         onupdate=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Romancista:
+    __tablename__ = 'romancistas'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    nome: Mapped[str] = mapped_column(unique=True)
+    livros: Mapped[list['Livro']] = relationship(
+        init=False, back_populates='autoria', cascade='all, delete-orphan'
+    )
+
+
+@table_registry.mapped_as_dataclass
+class Livro:
+    __tablename__ = 'livros'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    ano: Mapped[datetime]
+    titulo: Mapped[str] = mapped_column(unique=True)
+    id_romancista: Mapped[int] = mapped_column(ForeignKey('romancistas.id'))
+    autoria: Mapped[Romancista] = relationship(
+        init=False, back_populates='livros'
     )
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,5 @@
-from madr.models import User
+from datetime import datetime
+from madr.models import User, Livro, Romancista
 from sqlalchemy import select
 
 
@@ -15,3 +16,31 @@ def test_create_user(session):
     )
 
     assert result.username == 'romario'
+
+
+def test_create_romancista(session):
+
+    romancista = Romancista(nome='machado de assis')
+
+    session.add(romancista)
+    session.commit()
+    session.refresh(romancista)
+
+    assert romancista.id == 1
+
+
+def test_create_liro(session):
+
+    romancista = Romancista(nome='machado de assis')
+
+    session.add(romancista)
+    session.commit()
+    session.refresh(romancista)
+
+    livro = Livro(ano=datetime.now(), titulo='dom casmurro', id_romancista=1)
+
+    session.add(livro)
+    session.commit()
+    session.refresh(livro)
+
+    assert livro.id == 1
